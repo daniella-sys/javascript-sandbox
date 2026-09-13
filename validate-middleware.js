cc1b9daa3201001d4deba18520d2307c7b80866a
daniella-sys <hoohhausaufgaben@gmail.com>
2026-09-13 13:44:13 +0300

    Add validation middleware using Zod
    Implement middleware for validating request body using Zod schema.

---
+++ validate-middleware.js
@@ -0,0 +1,19 @@
+const { success } = require("zod");
+
+//middleware(validation)
+const validate = (schema) => (req, res, next) => {
+  try{
+    //парсимо і перевіряємо req.body
+    req.body = schema.parse(req.body); //перевіряє дані з тіла запиту за схемою
+    next();
+  }catch(error){
+    return res.status(400).json({
+      success: false,
+      errors: error.errors.map((err) => ({
+        field: err.path.join('.'),
+        message: err.message
+      })),
+    })
+  }
+}
+module.exports = {validate};
