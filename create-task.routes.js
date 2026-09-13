2363aeff8b831d652177059218f5229b4bdfc5e7
daniella-sys <hoohhausaufgaben@gmail.com>
2026-09-13 14:21:43 +0300

    Add task creation route with validation and auth
    Express router module establishing an execution pipeline for creating a new task (POST /api/task) by chaining authentication, schema validation, and database handler execution.

---
+++ create-task.routes.js
@@ -0,0 +1,21 @@
+//ПІДКЛЮЧАЄМО PRISMA і роутер створюємо
+ const express = require('express');
+ const router = express.Router();
+
+ // 1. Імпортуємо мідлвари
+const { authMiddleware } = require('../middlewares/auth.js');
+const { validate } = require('../middlewares/validate_middleware.js');
+
+//2. Імпортуємо Zod-схему
+const { createTaskSchema } = require('../schema/validation_schema.js');
+
+// 3. Імпортуємо контролер
+const {  createTask  } = require('../controllers/task.controller.js');
+ //роутер
+ router.post(
+    '/api/task',
+    authMiddleware, //перевірка авторизації 
+    validate(createTaskSchema), 
+     createTask//контролер
+ );
+ module.exports = router;
